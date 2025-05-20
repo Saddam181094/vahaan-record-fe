8bebabd249951f6a5cdf877b807d1f289a0aad33
Pheonix2507 <chintub2507@gmail.com>
2025-05-20 16:49:43 +0530

    CHECKING

--- src/pages/EmployeeDash.tsx
+++ src/pages/EmployeeDash.tsx
@@ -1,5 +1,5 @@
 const AdminDashboard = () => {
-  return <div className="p-6 text-xl">Welcome, Employee</div>;
+  return <div className="p-6 text-xl">Welcome, Employee!!</div>;
 };
 
 export default AdminDashboard;
